aa46f9cc6ef817fc2bea0135889b6c2820bd21e6
takehiro <takehiro-r133@hotmail.co.jp>
2021-09-21 16:33:27 +0900

    add profileImage component #25

---
+++ components/ProfileImage.vue
@@ -0,0 +1,26 @@
+<template>
+  <img
+    class="inline rounded-full"
+    :src="url"
+    alt="profile image"
+    @click="clickAction"
+  />
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'ProfileImage',
+  props: {
+    url: {
+      type: String,
+      requried: true,
+    },
+    clickAction: {
+      type: Function,
+      default: () => {},
+    },
+  },
+  setup() {},
+})
+</script>
